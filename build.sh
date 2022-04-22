9480bf02d9d3097e324aabdcbc02c4c07ff05fd3
Gilbert Gilb's <gilbsgilbert@gmail.com>
2022-04-22 19:46:05 +0200

    Add support for emoji 14.
    As GitHub's gemoji repository isn't very active and doesn't include
    latest emojis yet, I moved the emoji data source to
    muan/unicode-emoji-json which seems well maintained and up to date. Note
    that this also removes some punctuation marks from the emoji names, but
    it doesn't seem too bad.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i '/^### DATA ###$/q' wofi-emoji
 
-curl https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json \
-  | jq  --raw-output '.[] | (.emoji + " " + .description)' \
+curl https://raw.githubusercontent.com/muan/unicode-emoji-json/v0.3.1/data-by-emoji.json \
+  | jq --raw-output '. | to_entries | .[] | (.key  + " " + .value.name)' \
   >> wofi-emoji
