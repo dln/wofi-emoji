6f7ce8b878710a49656bb3a992f536fbdd429db6
Gilbert Gilb's <gilbsgilbert@gmail.com>
2022-06-01 15:15:42 +0200

    pin a specific version of emojilib
    This makes build more reproducible and will prevent random build
    breakages in the future.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i '/^### DATA ###$/q' wofi-emoji
 
-curl https://raw.githubusercontent.com/muan/emojilib/main/dist/emoji-en-US.json \
+curl https://raw.githubusercontent.com/muan/emojilib/v3.0.6/dist/emoji-en-US.json \
   | jq --raw-output '. | to_entries | .[] | .key + " " + (.value | join(" ") | sub("_"; " "; "g"))' \
   >> wofi-emoji
